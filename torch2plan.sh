c48a615436decc0c3e20953fbc736f166ff23e47
zl-cs <2787729270@qq.com>
2023-05-06 04:45:52 -0700

    update torch2plan.sh

--- torch2plan.sh
+++ torch2plan.sh
@@ -1,6 +1,10 @@
 # !/bin/bash
 
-model_list="yolov5n yolov5s yolov5m yolov5n6 yolov5s6 yolov5m6"
+#python3 -m pip install colored polygraphy --extra-index-url https://pypi.ngc.nvidia.com
+export POLYGRAPHY_AUTOINSTALL_DEPS=1  #let polygraphy install missing modules automatically
+
+#model_list="yolov5n yolov5s yolov5m yolov5n6 yolov5s6 yolov5m6"
+model_list="yolov8n"
 
 for model in $model_list
 do
@@ -8,5 +12,9 @@ do
     python3 pt2plan.py --weights $model.pt --include onnx
     echo "==================== $model.onnx to $model.plan ===================="
     /usr/src/tensorrt/bin/trtexec --onnx=$model.onnx --saveEngine=./plan/$model.plan --fp16
+    echo "==================== folding $model.onnx to $model\_folded.onnx ===================="
+    polygraphy surgeon sanitize $model.onnx --fold-constants --output $model\_folded.onnx    
+    echo "==================== send $model\_folded.onnx to nano-2 ===================="
+    scp $model\_folded.onnx nvidia@192.168.0.102:/home/nvidia/software/otherdisk/yolov5
 done
 
