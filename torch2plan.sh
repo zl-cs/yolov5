6a0c3d8b100934b3933940fe6ead2af4ef96c3df
nvidia <zl-cs>
2023-05-06 19:51:35 +0800

    add nano-2 repo with new torch2plan.sh

--- torch2plan.sh
+++ torch2plan.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-model_list="yolov5n yolov5s yolov5m yolov5n6 yolov5s6 yolov5m6"
-
+#model_list="yolov5n yolov5s yolov5m yolov5n6 yolov5s6 yolov5m6"
+model_list="yolov8n"
 for model in $model_list
 do
-    echo "==================== $model.pt to $model.onnx ===================="
-    python3 pt2plan.py --weights $model.pt --include onnx
+    #echo "==================== $model.pt to $model.onnx ===================="
+    #python3 pt2plan.py --weights $model.pt --include onnx
     echo "==================== $model.onnx to $model.plan ===================="
-    /usr/src/tensorrt/bin/trtexec --onnx=$model.onnx --saveEngine=./plan/$model.plan --fp16
+    /usr/src/tensorrt/bin/trtexec --onnx=$model\_folded.onnx --saveEngine=./plan/$model.plan --fp16
 done
 
---
+++ zl_requirements.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+pip3 install psutil tqdm seaborn                                                           
+python3 -m pip install colored polygraphy --extra-index-url https://pypi.ngc.nvidia.com
