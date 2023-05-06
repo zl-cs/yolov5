41f5dad9c331e5177f9c94b174c3d2a434213643
nvidia <zl-cs>
2023-05-06 19:53:11 +0800

    delete polygraphy install in zl_requirements.sh

--- zl_requirements.sh
+++ zl_requirements.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
 pip3 install psutil tqdm seaborn                                                           
-python3 -m pip install colored polygraphy --extra-index-url https://pypi.ngc.nvidia.com
